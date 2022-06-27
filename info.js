4627ceb8b2a0a5349927568ca37f532a64e703af
danny-jin <icarus96119@gmail.com>
2022-06-27 17:45:32 +0900

    initial commit

---
+++ info.js
@@ -0,0 +1,351 @@
+let info = {
+  name: 'Danny Jin',
+  logo_name: 'Danny JIn',
+  flat_picture: require('./src/assets/avatar.png'),
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false
+    }
+  },
+  description:
+    'Accomplished a senior blockchain developer with 4+ years of hands-on experience in delivering top-notch decentralized applications and troubleshooting for many startups and firms. Focused on creating collaborative efforts between support and engineering teams to implement system or product level changes to benefit end-users. Eager to support building an impeccable online reputation by providing high-level customer support for startups and firms. With in-depth knowledge of Artificial Intelligence, implemented deep learning to forecast Cryptocurrency price and made market-competitive tokens.',
+  links: {
+    github: 'https://github.com/danny-jin',
+    linkedin: 'https://www.linkedin.com/in/danny-jin-378ab41a8',
+    resume:
+      'https://drive.google.com/file/d/1c4xIX2Wo0OwIa6zy_q3krUHg7aIGd-Yj/view?usp=sharing'
+    ,
+    discord:
+      'https://discord.gg/vFgsnNHz'
+    ,
+    telegram:
+      'https://t.me/sacred_danny'
+  },
+  education: [
+    {
+      name: 'National University of Singapore',
+      place: 'Singapore',
+      date: 'Nov, 2011 - Aug, 2015',
+      degree: 'Masters in Computer Science',
+      description:
+        'Currently pursuing my Master\'s in Computer Science, while specializing in the field of Artificial Intelligence and Machine Learning.',
+      skills: [
+        'Artificial Intelligence',
+        'Software Engineering',
+        'Algorithms',
+        'Computer Vision',
+        'Music Data Mining',
+        'OS'
+      ]
+    }
+  ],
+  experience: [
+    {
+      name: 'Full Stack Developer',
+      place: 'Ariswap',
+      date: 'Oct, 2021 - Apr, 2022',
+      position: '',
+      description:
+        'Develop a Celo native NFT Marketplace focused on creating the conditions of prosperity for all through NFT & Art',
+      skills: ['Blockchain', 'Smart contracts', 'Solidity', 'Web3.js', 'React.js', 'Typescript']
+    },
+    {
+      name: 'Solana Developer',
+      place: 'Chip',
+      date: 'Jun 2021 — October 2021',
+      position: '',
+      description:
+        'This project(BabyPunks) is a limited NFT collection on the Solana blockchain.\n' +
+          'Supply is capped at 10,000.Your BabyPunks allow you to earn 5% royalties paid in SOL tokens from\n' +
+          'every buy & sell for life.',
+      skills: ['NFT', 'Smart contracts', 'Rust', 'Solana', 'Typescript', 'Node.js', 'Arweave']
+    },
+    {
+      name: 'Smart Contract Developer',
+      place: 'WagyuSwap',
+      date: 'May 2021 — Oct 2021',
+      position: '',
+      description:
+        'This project is the leading decentralized exchange on the Velas Chain, which has been clocked as the\n' +
+          'fastest blockchain in the world.',
+      skills: ['Blockchain', 'Smart contracts', 'Solidity', 'React.js', 'Web3.js']
+    },
+    {
+      name: 'Lead Defi Engineer',
+      place: 'Chipshop',
+      date: 'Jan 2021 — May 2021',
+      position: '',
+      description:
+        'Chipshop Finance is an ambitious experimental economy project on the Binance Smart Chain (BSC).\n' +
+          'The aim of this project is to become the longest lasting and most successful stablecoin on the BSC,\n' +
+          'learning from the mistakes of others which went under-pegged. It is a fork of Wantan Mee, a lightweight\n' +
+          'implementation of the Basis Protocol on Ethereum.',
+      skills: ['Blockchain', 'Smart contracts', 'Solidity', 'Web3.js', 'React']
+    },
+    {
+      name: 'Full Stack Developer',
+      place: 'Vavuris',
+      date: 'Feb 2020 — Nov 2020',
+      position: '',
+      description:
+          'This project is a SAAS platform for landscaping',
+      skills: ['Next Js', 'Nest Js', 'Angular', 'AWS']
+    },
+    {
+      name: 'Blockchain Backend Engineer',
+      place: 'Gunthy',
+      date: 'Jan 2019 — Mar 2020',
+      position: '',
+      description:
+          'The difference between manual trading and trading with Gunbot is your emotions. Trade 24/7, increase\n' +
+          'your profits, and don’t let your emotions trade for you.\n' +
+          'Gunthy offers a complete solution for crypto trading. installed on your own computer or server.\n' +
+          'See which trades were recently made, or are about to happen. Get push notifications on Telegram.\n' +
+          'Edit your strategy or pairs from any device. Useful charts with the exact data Gunbot works with.\n' +
+          'Backtest Gunbot strategies on TradingView, for any time frame or market',
+      skills: ['Blockchain', 'Smart contracts', 'Solidity', 'Web3', 'Node']
+    },
+    {
+      name: 'Project Lead Developer',
+      place: 'Taks',
+      date: 'Jan 2021 — May 2021',
+      position: '',
+      description:
+          'This project is a Saas platform for Task management.',
+      skills: ['Android', 'ios', 'Mac', 'Windows', 'Web']
+    },
+    {
+      name: 'Senior React/Redux Developer',
+      place: 'Doxel',
+      date: 'Jul 2026 — Aug 2017',
+      position: '',
+      description:
+          'Built front-end with React.js/Redux and Material UI library, implemented some advanced features\n' +
+          'like Panorama 360 viewer, and integrated third-party libraries like Autodesk Forge Viewer, Leaflet.js,\n' +
+          'Mixpanel, Rollbar, etc.',
+      skills: ['React', 'HTML5', 'CSS3', 'Redux', 'Web']
+    }
+  ],
+  skills: [
+    {
+      title: 'languages',
+      info: [
+        'C',
+        'C++',
+        'Java',
+        'Python',
+        'Javascript',
+        'TypeScript',
+        'SQL',
+        'NoSQL',
+        'Ruby',
+        'Solidity',
+        'Rust',
+        'Go/Golang'
+      ],
+      icon: 'fa fa-code'
+    },
+    {
+      title: 'data frameworks',
+      info: [
+        'Keras',
+        'Numpy',
+        'Pandas',
+        'OpenCV',
+        'SciPy',
+        'Sklearn',
+        'LibROSA',
+        'Pillow'
+      ],
+      icon: 'fa fa-cubes'
+    },
+    {
+      title: 'web technologies',
+      info: ['Vue', 'Angular', 'React', 'Node', 'Flask', 'HTML', 'CSS'],
+      icon: 'fas fa-laptop-code'
+    },
+    {
+      title: 'Blockchain technologies',
+      info: ['Smart contracts', 'Solidity', 'Rust', 'Ether.js', 'Web3.js', 'Typescript', 'Javascript', 'Hardhat/Truffle/Remix', 'Wax'],
+      icon: 'fas fa-laptop-code'
+    },
+    {
+      title: 'databases',
+      info: ['MongoDB', 'MySQL', 'OracleSQL', 'Neo4j', 'PostgreSQL'],
+      icon: 'fa fa-database'
+    },
+    {
+      title: 'operating systems & tools',
+      info: [
+        'MacOS',
+        'Ubuntu',
+        'Windows',
+        'Android',
+        'Agile',
+        'Scrum',
+        'JIRA',
+        'Heroku',
+        'Firebase',
+        'AWS S3'
+      ],
+      icon: 'fas fa-tools'
+    }
+  ],
+  portfolio: [
+    {
+      name: 'Ariswap Nft Marketplace',
+      pictures: [
+        {
+          img: require('./src/assets/portfolio/ariswap/one.png')
+        },
+        {
+          img: require('./src/assets/portfolio/ariswap/two.png')
+        }
+      ],
+      technologies: [
+        'React',
+        'Tailwind CSS',
+        'Typescript',
+        'EVM network',
+        'Solidity',
+        'Web3.js'
+      ],
+      category: 'NFT Marketplace Dapp',
+      github: 'https://github.com/danny-jin/ariswap_frontend',
+      date: 'Oct, 2021 - Apr, 2022',
+      visit: 'https://ariswap.co/',
+      description:
+          'Develop a Celo native NFT Marketplace focused on creating the conditions of prosperity for all through NFT & Art'
+    },
+    {
+      name: 'Templar Dao(Olympus Dao fork)',
+      pictures: [
+        {
+          img: require('./src/assets/portfolio/templar/templar-1.jpg')
+        },
+        {
+          img: require('./src/assets/portfolio/templar/templar-2.jpg')
+        },
+        {
+          img: require('./src/assets/portfolio/templar/templar-3.jpg')
+        },
+        {
+          img: require('./src/assets/portfolio/templar/templar-4.jpg')
+        }
+      ],
+      technologies: ['Solidity', 'Typescript', 'React', 'Tailwind CSS', 'GrapQL', 'Redux'],
+      category: 'Dao Dapp',
+      date: 'Aug, 2021 - Jan, 2022',
+      github:
+        'https://github.com/TemplarDAO',
+      visit: 'https://templar.finance',
+      description:
+        'Templar is the first decentralized reserve currency protocol available on the Binance Smart Chain based on the TEM token. Each TEM token is backed by a basket of assets (e.g. BNB, BUSD, TEM-BUSD LP) in the Templar treasury, giving it an intrinsic value that it cannot fall below. Templar also introduces unique economic and game-theoretic dynamics into the market through staking and bonding.'
+    },
+    {
+      name: 'WagyuSwap',
+      pictures: [
+        {
+          img: require('./src/assets/portfolio/wagyu/logo.png')
+        },
+        {
+          img: require('./src/assets/portfolio/wagyu/one.png')
+        },
+        {
+          img: require('./src/assets/portfolio/wagyu/two.png')
+        },
+        {
+          img: require('./src/assets/portfolio/wagyu/three.png')
+        },
+      ],
+      technologies: ['React', 'Solidity', 'Web3.js', 'Git'],
+      category: 'Dex Dapp',
+      date: 'May, 2021 - Oct, 2021',
+      github: 'https://github.com/wagyuswapapp',
+      visit: 'https://www.wagyuswap.app/',
+      description:
+        'This project is the leading decentralized exchange on the Velas Chain, which has been clocked as the\n' +
+          'fastest blockchain in the world.'
+    },
+  ],
+  portfolio_web: [
+    {
+      name: 'Vavuris Landscaping',
+      title: 'Web development',
+      pictures: [
+        {
+          img: require('./src/assets/portfolio/vavuris/home.png'),
+          title: 'Home'
+        },
+        {
+          img: require('./src/assets/portfolio/vavuris/one.png'),
+          title: 'What is vavuris'
+        },
+        {
+          img: require('./src/assets/portfolio/vavuris/two.png'),
+          title: 'How to work'
+        },
+        {
+          img: require('./src/assets/portfolio/vavuris/three.png'),
+          title: 'Order'
+        }
+      ],
+      technologies: ['Next.js', 'Angular', 'Rxjs', 'Tailwind CSS', 'Typescript', 'Nest.js', 'PostgreSQL', 'AWS', 'Git'],
+      category: 'Full stack development',
+      github: '',
+      date: 'Feb, 2020 - Nov, 2020',
+      visit: '',
+      description:
+        'Hardscape Architect will consult you about your project and create a detailed 2D Digital Blueprint so that you could get an overall idea of material costs.'
+    },
+    {
+      name: 'Doxel',
+      title: 'AI Powered Project',
+      pictures: [
+        {
+          img: require('./src/assets/portfolio/doxel/logo.png'),
+        },
+        {
+          img: require('./src/assets/portfolio/doxel/one.png'),
+        },
+        {
+          img: require('./src/assets/portfolio/doxel/two.png'),
+        },
+        {
+          img: require('./src/assets/portfolio/doxel/three.png'),
+        }
+      ],
+      technologies: ['React', 'SASS/SCSS', 'React Native', 'MySQL', 'HTML', 'Electron Js'],
+      category: 'Web development',
+      github: '',
+      date: 'Apr, 2017 - Feb, 2019',
+      visit: '',
+      description:
+        'Continuous performance monitoring and predictive insights to stay one step ahead of delays and cost overruns.'
+    }
+  ],
+  recommendations: [
+    {
+      title:
+        'Last year, I\'ve made a decision to make the longest-lasting and most successful stable coin on the BSC. So I\'ve posted a job on Upwork and hired Danny.\n' +
+          'He is the most skillful engineer I\'ve ever seen. He is awesome at developing smart contracts. And then he also has a pack of expensive experiences with full-stack development.\n' +
+          'Actually, he built my project from scratch himself.\n' +
+          'I\'d highly recommend Danny as a senior full-stack and blockchain engineer.',
+      author: 'Reece Mander',
+      position: 'CEO',
+      company: 'Chipshop',
+      location: 'London, England'
+    },
+    {
+      title:
+        'During the course of his employment we have found him to be a self-started who was motivated, duty bound and a highly commited team player.',
+      author: 'Alex Rodriguez',
+      position: 'CTO',
+      company: 'Ariswap',
+      location: 'San Diego, California'
+    }
+  ]
+};
+
+export default info;
